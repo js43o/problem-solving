89dbd9b769f4f94ab4175b421744559612f45d09
Jiseung <50646827+js43o@users.noreply.github.com>
2023-08-17 15:25:38 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+print(sum([a * b for (a, b) in zip(A, B)]))
