cd1086cb432f9f382e7c59703a687c3c4198599f
Jiseung <50646827+js43o@users.noreply.github.com>
2023-08-18 09:07:54 +0900

    [Gold V] Title: 용액, Time: 488 ms, Memory: 42176 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+minValue = 10**10
+result = None
+
+
+def binary_search(start, end, value):
+    global minValue, result
+
+    if start >= end:
+        return
+
+    mid = (start + end) // 2
+    if value == arr[mid]:
+        return
+
+    newValue = value + arr[mid]
+
+    if abs(newValue) < minValue:
+        minValue = abs(newValue)
+        result = (value, arr[mid])
+
+    if newValue > 0:
+        binary_search(start, mid, value)
+    elif newValue < 0:
+        binary_search(mid + 1, end, value)
+    else:
+        return
+
+
+for value in arr:
+    binary_search(0, N, value)
+
+print(*sorted(result))
