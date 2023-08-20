b9307056cc6a509c401c06e6a081fbad2e9bdc96
Jiseung <50646827+js43o@users.noreply.github.com>
2023-08-20 17:03:29 +0900

    [Gold III] Title: 앱, Time: 620 ms, Memory: 43872 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -5,7 +5,8 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 mems = [0] + list(map(int, input().split()))
 costs = [0] + list(map(int, input().split()))
-dp = [[0] * (N + 1) for _ in range(10001)]  # dp[앱 인덱스][비용] = 확보된 메모리
+dp = [[0] * (N + 1) for _ in range(10001)]
+# dp[i][k] =  최대 비용이 k인 경우, i번째 앱까지 고려했을 때의 최대 메모리 바이트 값
 
 for totalCost in range(10001):
     for idx in range(1, N + 1):
