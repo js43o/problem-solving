734b86ec43c45f3c87e272c10cdee1370cffce29
Jiseung <50646827+js43o@users.noreply.github.com>
2023-08-22 12:05:11 +0900

    [Silver III] Title: N과 M (3), Time: 500 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#define MAX 9
+using namespace std;
+
+int n, m;
+int arr[MAX];
+
+void func(int count) {
+	if (count == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		arr[count] = i;
+		func(count + 1);
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	func(0);
+}
