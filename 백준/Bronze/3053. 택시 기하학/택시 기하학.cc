69f6e146c91f854663946966898b31eddc0805f3
Jiseung <50646827+js43o@users.noreply.github.com>
2023-08-24 13:07:59 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double r;
+	cin >> r;
+	cout << fixed;
+	cout.precision(6);
+	cout << r * r * M_PI << endl << 2 * r * r << endl;
+}
