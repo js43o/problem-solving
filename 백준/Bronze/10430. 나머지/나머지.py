7526a4d77ada72c35e4b2a3ad92e0a18fd8c68df
Jiseung <50646827+js43o@users.noreply.github.com>
2023-08-24 13:11:37 +0900

    [Bronze V] Title: 나머지, Time: 60 ms, Memory: 29056 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,6 @@
+a = input()
+b = a.split(' ')
+print((int(b[0]) + int(b[1])) % int(b[2]))
+print((int(b[0]) % int(b[2]) + int(b[1]) % int(b[2]) ) % int(b[2]))
+print((int(b[0]) * int(b[1])) % int(b[2]))
+print((int(b[0]) % int(b[2]) * int(b[1]) % int(b[2]) ) % int(b[2]))
