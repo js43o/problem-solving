a2b082581479e15508b202497712ea8688733092
Jiseung <js43og@gmail.com>
2023-12-26 19:32:07 +0900

    [level 2] Title: 모음 사전, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.43 ms
+메모리: 33.4 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 18:54:6
+2023년 12월 2일 19:32:5
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,25 +1,12 @@
-const letters = ['A', 'E', 'I', 'O', 'U'];
-let answer = -1;
-let count = 0;
-
-function dfs(cur, target) {
-    if (cur === target) {
-        answer = count;
-        return;
-    }
-    
-    count += 1;
-    
-    if (cur.length >= 5) {
-        return;
-    }
-
-    for (let i = 0; i < 5; i++) {
-        dfs(cur + letters[i], target);
-    }
-}
 
 function solution(word) {
-    dfs('', word);
+    const gap = [781, 156, 31, 6, 1];
+    const face = ['A', 'E', 'I', 'O', 'U'];
+    let answer = word.length;
+    
+    Array.from(word).forEach((w, i) => {
+        answer += gap[i] * face.indexOf(w);
+    });
+    
     return answer;
 }
