ca989c04c9de3a1dcd50509be940acea1b9238b9
Jiseung <js43og@gmail.com>
2024-01-10 21:43:51 +0900

    Update 택배상자.js

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,12 +1,10 @@
 function solution(order) {
-    let answer = 0;
     const temp = [];
     let orderIdx = 0;
     let curBox = 1;
     
-    while (orderIdx < order.length && curBox <= order.length) {
+    while (orderIdx < order.length) {
         if (order[orderIdx] === curBox) {
-            answer += 1;
             curBox += 1;
             orderIdx += 1;
             continue;
@@ -14,20 +12,17 @@ function solution(order) {
         
         if (temp.length > 0 && temp[temp.length - 1] === order[orderIdx]) {
             temp.pop();
-            answer += 1;
             orderIdx += 1;
             continue;
+        }
+        
+        if (curBox > order.length && temp.length > 0 && temp[temp.length - 1] !== order[orderIdx]) {
+            break;
         }
         
         temp.push(curBox);
         curBox += 1;
     };
     
-    while (orderIdx < order.length && temp.length && temp[temp.length - 1] === order[orderIdx]) {
-        temp.pop();
-        answer += 1;
-        orderIdx += 1;
-    }
-    
-    return answer;
+    return orderIdx;
 }
